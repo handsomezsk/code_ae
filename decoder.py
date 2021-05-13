70494d8baa06fd289c1822bb6e0e515fbc1762e0
handsomezsk <2294146957@qq.com>
2021-05-13 22:41:24 +0800

    just change forms

--- decoder.py
+++ decoder.py
@@ -36,7 +36,7 @@ class DEC(torch.nn.Module):
         self.fc = torch.nn.Linear(2*args.dec_num_unit, args.dec_num_unit)
 
         self.attn = torch.nn.Linear(2*args.dec_num_unit, args.dec_num_unit, bias=False)
-        self.v = torch.nn.Linear(args.dec_num_unit, 1, bias=False)
+        self.v = torch.nn.Linear(args.dec_num_unit, 2, bias=False)
 
     def dec_act(self, inputs):
         if self.args.dec_act == 'tanh':
@@ -56,7 +56,7 @@ class DEC(torch.nn.Module):
 
     def attention(self, s, hidden):
 
-        src_len = hidden.shape[1]
+        src_len = int(hidden.shape[1]/2)
 
         # repeat decoder hidden state src_len times
         # s = [batch_size, src_len, dec_hid_dim]
@@ -65,35 +65,28 @@ class DEC(torch.nn.Module):
         # energy = [batch_size, src_len, dec_hid_dim]
         energy = torch.tanh(self.attn(torch.cat((s, hidden), dim=2)))
         # attention = [batch_size, src_len]
-        attention = self.v(energy).squeeze(2)
-        return F.softmax(attention, dim=1)
+        attention = self.v(energy)
+        attention = attention.transpose(1, 2)
+        return F.softmax(attention, dim=2)
 
     def forward(self, received):
         received = received.type(torch.FloatTensor).to(self.this_device)
         for i in range(self.args.block_len):
-            if i==0:
+            if i == 0:
                 out1, hidden1 = self.dec1_rnns(received[:, i:i+1, :])
                 out2, hidden2 = self.dec2_rnns(received[:, i:i+1, :])
-                hidden1_tra=hidden1.transpose(0, 1)
-                hidden2_tra=hidden2.transpose(0, 1)
-                hiddens11 = hidden1_tra[:, 0:1, :]
-                hiddens12 = hidden1_tra[:, 1:2, :]
-                hiddens21 = hidden2_tra[:, 0:1, :]
-                hiddens22 = hidden2_tra[:, 1:2, :]
-                a11 = self.attention(hidden1_tra[:, 0:1, :], hiddens11).unsqueeze(1)
-                a12 = self.attention(hidden1_tra[:, 1:2, :], hiddens12).unsqueeze(1)
-                a21 = self.attention(hidden2_tra[:, 0:1, :], hiddens21).unsqueeze(1)
-                a22 = self.attention(hidden2_tra[:, 1:2, :], hiddens22).unsqueeze(1)
+                hidden1_tra = hidden1.transpose(0, 1)
+                hidden2_tra = hidden2.transpose(0, 1)
+                hiddens1 = hidden1_tra
+                hiddens2 = hidden2_tra
+                a1 = self.attention(hidden1_tra, hiddens1)
+                a2 = self.attention(hidden2_tra, hiddens2)
                 
-                c11 = torch.bmm(a11, hiddens11)
-                c12 = torch.bmm(a12, hiddens12)
-                c1 = torch.cat((c11, c12),dim=1)
+                c1 = torch.bmm(a1, hiddens1)
                 c1 = c1.transpose(0, 1)
                 hidden1 = torch.cat((c1, hidden1), dim=2)
                 hidden1 = self.fc(hidden1)
-                c21 = torch.bmm(a21, hiddens21)
-                c22 = torch.bmm(a22, hiddens22)
-                c2 = torch.cat((c21, c22),dim=1)
+                c2 = torch.bmm(a2, hiddens2)
                 c2 = c2.transpose(0, 1)
                 hidden2 = torch.cat((c2, hidden2), dim=2)
                 hidden2 = self.fc(hidden2)
@@ -105,24 +98,16 @@ class DEC(torch.nn.Module):
                 out2, hidden2 = self.dec2_rnns(received[:, i:i+1, :], hidden2)
                 hidden1_tra = hidden1.transpose(0, 1)
                 hidden2_tra = hidden2.transpose(0, 1)
-                hiddens11 = torch.cat((hiddens11,hidden1_tra[:, 0:1, :]),dim=1)
-                hiddens12 = torch.cat((hiddens11,hidden1_tra[:, 1:2, :]),dim=1)
-                hiddens21 = torch.cat((hiddens21,hidden2_tra[:, 0:1, :]),dim=1)
-                hiddens22 = torch.cat((hiddens21,hidden2_tra[:, 1:2, :]),dim=1)
-                a11 = self.attention(hidden1_tra[:, 0:1, :], hiddens11).unsqueeze(1)
-                a12 = self.attention(hidden1_tra[:, 1:2, :], hiddens12).unsqueeze(1)
-                a21 = self.attention(hidden2_tra[:, 0:1, :], hiddens21).unsqueeze(1)
-                a22 = self.attention(hidden2_tra[:, 1:2, :], hiddens22).unsqueeze(1)
+                hiddens1 = torch.cat((hiddens1, hidden1_tra), dim=1)
+                hiddens2 = torch.cat((hiddens2, hidden2_tra), dim=1)
+                a1 = self.attention(hidden1_tra, hiddens1)
+                a2 = self.attention(hidden2_tra, hiddens2)
 
-                c11 = torch.bmm(a11, hiddens11)
-                c12 = torch.bmm(a12, hiddens12)
-                c1 = torch.cat((c11, c12), dim=1)
+                c1 = torch.bmm(a1, hiddens1)
                 c1 = c1.transpose(0, 1)
                 hidden1 = torch.cat((c1, hidden1), dim=2)
                 hidden1 = self.fc(hidden1)
-                c21 = torch.bmm(a21, hiddens21)
-                c22 = torch.bmm(a22, hiddens22)
-                c2 = torch.cat((c21, c22), dim=1)
+                c2 = torch.bmm(a2, hiddens2)
                 c2 = c2.transpose(0, 1)
                 hidden2 = torch.cat((c2, hidden2), dim=2)
                 hidden2 = self.fc(hidden2)
